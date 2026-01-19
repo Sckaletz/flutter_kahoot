eaf2f237dc13cbdec7ebfc508e68634fdc284deb
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2026-01-19 13:34:18 +0100

    LeaderboardModel

---
+++ lib/models/leaderboard.dart
@@ -0,0 +1,23 @@
+class Leaderboard {
+  final int participantId;
+  final String nickname;
+  final int totalPoints;
+  final int rank;
+
+  Leaderboard({
+    required this.participantId,
+    required this.nickname,
+    required this.totalPoints,
+    required this.rank,
+  });
+
+  // Opretter en Leaderboard instans fra JSON data
+  factory Leaderboard.fromJson(Map<String, dynamic> json) {
+    return Leaderboard(
+      participantId: json['participantId'] ?? 0,
+      nickname: json['nickname'] ?? '',
+      totalPoints: json['totalPoints'] ?? 0,
+      rank: json['rank'] ?? 0,
+    );
+  }
+}
